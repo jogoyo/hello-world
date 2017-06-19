7b328e877c4421306fcce5be765c7037ee954747
Joseph Di Pasquale <joseph@josephdp.com>
2017-06-18 20:00:20 -0400

    Added ability to add numbers

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -17,8 +17,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-        print(text1.text!)
-        print(text2.text!)
+        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
     }
 
     
