556bff34397d913a8798bfd185a17d660d063ed6
Joseph Di Pasquale <joseph@josephdp.com>
2017-06-18 20:23:44 -0400

    Added subtraction ability based on boolean value + new answer emojis

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -17,20 +17,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-        theLabel.text = "Answer: \(Double(text1.text!)! + Double(text2.text!)!)"
+        let addition = false
+        if addition {
+            theLabel.text = "▶️ \(Double(text1.text!)! + Double(text2.text!)!) ◀️"
+        } else {
+            theLabel.text = "▶️ \(Double(text1.text!)! - Double(text2.text!)!) ◀️"
+        }
     }
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
